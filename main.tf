d2ab327d03bb316fdbd719712797adf6599a0d42
Greugreu <fenris.fitz@hotmail.fr>
2023-03-22 11:46:31 +0100

    Fix service plan && web app && more variables

--- main.tf
+++ main.tf
@@ -13,14 +13,24 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "TPAzureGroup" {
-  location = "West Europe"
-  name     = "rg-vclarke${var.environment_suffix}"
+  location = var.location
+  name     = "rg-${var.projectName}${var.environment_suffix}"
 }
 
 resource "azurerm_service_plan" "AzurermServicePlan" {
-  name                = "exmaple-network"
-  resource_group_name = azurerm_resource_group.rg-vclarke.name
-  location            = azurerm_resource_group.rg_vclarke.location
+  name                = "${var.projectName}${var.environment_suffix}-Plan"
+  resource_group_name = azurerm_resource_group.TPAzureGroup.name
+  location            = azurerm_resource_group.TPAzureGroup.location
   os_type             = "Linux"
   sku_name            = "P1v2"
+}
+
+resource "azurerm_linux_web_app" "AzurermWebApp" {
+  name                = "web-${var.projectName}${var.environment_suffix}"
+  resource_group_name = azurerm_resource_group.TPAzureGroup.name
+  location            = azurerm_resource_group.TPAzureGroup.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+  service_plan_id     = azurerm_resource_group.TPAzureGroup.id
+  site_config {}
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@
   type = string
   description = "Procure le suffixe "
 }
+
+variable "location" {
+  type = string
+  description = "West Europe"
+}
+
+variable "projectName" {
+  type = string
+  description = "vclarke"
+}
