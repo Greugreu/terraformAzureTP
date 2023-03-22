b0eae7c431c684f5f2ee92886367ea18ebe91765
Greugreu <fenris.fitz@hotmail.fr>
2023-03-22 11:32:31 +0100

    service plan && variables

--- main.tf
+++ main.tf
@@ -14,5 +14,13 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "TPAzureGroup" {
   location = "West Europe"
-  name     = "rg-vclarke"
+  name     = "rg-vclarke${var.environment_suffix}"
+}
+
+resource "azurerm_service_plan" "AzurermServicePlan" {
+  name                = "exmaple-network"
+  resource_group_name = azurerm_resource_group.rg-vclarke.name
+  location            = azurerm_resource_group.rg_vclarke.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
 }
---
+++ variables.tf
@@ -0,0 +1,4 @@
+﻿variable "environment_suffix" {
+  type = string
+  description = "Procure le suffixe "
+}
